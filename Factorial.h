20c8ad7cba7adddf67e28474bb7ae4f210a8096f
Laura Danniela Zárate <66334807+LauZar12@users.noreply.github.com>
2022-04-08 15:59:08 -0500

    Add files via upload

---
+++ Factorial.h
@@ -0,0 +1,10 @@
+//by Lau :D
+class Factorial{
+    private:
+        int numero;
+    public:
+        Factorial(int _numero){
+            _numero = numero;
+        }
+        void setFactorial(int _numero);
+}
