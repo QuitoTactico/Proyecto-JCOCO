0632021515d97a9c067e89b8f51fc1ad59e3e52e
Laura Danniela Zárate <66334807+LauZar12@users.noreply.github.com>
2022-04-08 15:58:37 -0500

    Add files via upload

---
+++ Calculadora.h
@@ -0,0 +1,13 @@
+//by Lau :D
+class Calculadora{
+    private:
+        int numero1;
+        int numero2;
+    public:
+        Calculadora(int _numero1, int _numero2){
+            _numero1 = numero1;
+            _numero2 = numero2;
+        }
+        void setNumer01(int _numero1);
+        void setNumero2(int _numero2);
+}
