dd03f5b9e14bc0ccf2bf582addca677d4852d9a3
Laura Danniela Zárate <66334807+LauZar12@users.noreply.github.com>
2022-04-15 03:08:27 -0500

    Add files via upload

--- Pascal.cpp
+++ Pascal.cpp
@@ -1,6 +1,45 @@
-//holi, holi, holi, i´m dying
 #include "Pascal.h"
+#include <iostream>
+#include <string.h>
+#include <fstream>
+using namespace std;
 
-void Pascal::setPascal(int pascal){
-    numero = pascal;
+
+int Pascal::generarArchivo(){
+    
+    ifstream plantilla("Pascal_plantilla.txt", ios::in);
+    ofstream ejecutable("Pascal_ejecutable.casm", ios::out);
+    
+    bool hecho = false;
+
+    while (!plantilla.eof()) {   //eof = end of file
+        string linea;
+        getline(plantilla, linea);
+        ejecutable << linea << endl;
+
+        if(hecho == false){  
+            ejecutable << "Constants: None, " << numero << endl; 
+            hecho = true; 
+        }
+    }
+    plantilla.close();
+    ejecutable.close();
+    return 0;
+}
+
+int Pascal::ejecutarArchivo(){
+    system("java -jar JCoCo.jar Pascal_ejecutable.casm");
+    return 0;
+}
+
+int Pascal::imprimirArchivo(){
+    ifstream imprimir("Pascal_ejecutable.casm", ios::in);
+
+    cout << imprimir.rdbuf();
+    imprimir.close();
+    return 0;
+}
+
+Pascal::Pascal(int _numero){
+    numero = _numero;
 }
