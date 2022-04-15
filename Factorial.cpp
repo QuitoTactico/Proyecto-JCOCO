cf6824dedb0f9654522dc85edec815465eb3288d
Laura Danniela Zárate <66334807+LauZar12@users.noreply.github.com>
2022-04-15 03:08:41 -0500

    Add files via upload

--- Factorial.cpp
+++ Factorial.cpp
@@ -1,6 +1,44 @@
 #include "Factorial.h"
+#include <iostream>
+#include <string.h>
+#include <fstream>
+using namespace std;
 
-void Factorial::setFactorial(int factorial){
-    numero = factorial;
+int Factorial::generarArchivo(){
+    
+    ifstream plantilla("Factorial_plantilla.txt", ios::in);
+    ofstream ejecutable("Factorial_ejecutable.casm", ios::out);
+    
+    bool hecho = false;
+
+    while (!plantilla.eof()) {   //eof = end of file
+        string linea;
+        getline(plantilla, linea);
+        ejecutable << linea << endl;
+
+        if(hecho == false){  
+            ejecutable << "Constants: None, " << numero << endl; 
+            hecho = true; 
+        }
+    }
+    plantilla.close();
+    ejecutable.close();
+    return 0;
 }
-//is this ok?
+
+int Factorial::ejecutarArchivo(){
+    system("java -jar JCoCo.jar Factorial_ejecutable.casm");
+    return 0;
+}
+
+int Factorial::imprimirArchivo(){
+    ifstream imprimir("Factorial_ejecutable.casm", ios::in);
+
+    cout << imprimir.rdbuf();
+    imprimir.close();
+    return 0;
+}
+
+Factorial::Factorial(int _numero){
+    numero = _numero;
+}
